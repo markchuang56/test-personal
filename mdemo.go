a897369a096a4183d7c2ee069681627633cbfafc
jasonchuang56 <jchuang@health2sync.com>
2018-11-13 19:50:29 +0800

    heroku

--- mdemo.go
+++ mdemo.go
@@ -258,6 +258,16 @@ func main() {
 		c.HTML(http.StatusOK, "loggedout.tmpl.html", nil)
 	})
 
+	router.POST("/post", func(c *gin.Context) {
+
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		name := c.PostForm("name")
+		message := c.PostForm("message")
+
+		fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+	})
+
 	router.Run(":" + portx)
 
 }
